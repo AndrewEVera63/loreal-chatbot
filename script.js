afa18cf0d7216425b58b64d0f8e1bec1af67a981
Andrew Vera <Aev63@txstate.edu>
2026-07-16 20:20:52 +0000

    Changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const chatForm = document.getElementById("chatForm");
 const userInput = document.getElementById("userInput");
 const chatWindow = document.getElementById("chatWindow");
-
+const workerUrl = 'https://loral-chatbot.andrewevera63.workers.dev/';
 // Set initial message
 chatWindow.textContent = "👋 Hello! How can I help you today?";
 
@@ -16,3 +16,5 @@ chatForm.addEventListener("submit", (e) => {
   // Show message
   chatWindow.innerHTML = "Connect to the OpenAI API for a response!";
 });
+
+
